3ca7ad9b3be1d7e59ad8fe2cead3497874fc9f5a
Christopher Young <christopheryoung@Christophers-MacBook-Pro.local>
2023-03-06 20:09:25 +0200

    HTML working and css working. Styles are now working.

---
+++ app.js
@@ -0,0 +1,18 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+
+
+const app = express();
+app.use(express.static(__dirname + '/public'));
+
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
